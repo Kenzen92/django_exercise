11f96c7c9244bb83e7fbe60430028ee26a69922d
kenzen92 <jamespeterkenny@googlemail.com>
2023-10-22 20:29:59 +0100

    trying to use forms need to use admin dashboard to see if its working

--- exercise_project/exercise_app/forms.py
+++ exercise_project/exercise_app/forms.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django.urls import reverse_lazy
-# from .models import Exercise
+from .models import Exercise
 
 subjectChoices = (
     (1, 'Web development'),
@@ -11,20 +11,15 @@ subjectChoices = (
     (3, 'Skullduggery')
 )
 
-class AddExerciseForm(forms.Form):
+class AddExerciseForm(forms.ModelForm):
+    class Meta:
+        model = Exercise
+        fields = ['name', 'description', 'category', 'duration', 'date']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.form_action = reverse_lazy('index')
         self.helper.add_input(Submit('submit', 'Submit'))
-
-
-    name = forms.CharField()
-    age = forms.IntegerField()
-    subject_choice = forms.ChoiceField(
-    choices = subjectChoices,
-    widget= forms.RadioSelect)
-    date_of_birth = forms.DateField(
-        widget=forms.DateInput(attrs={'type': 'date', 'max': datetime.now().date()})
-    )
+        # Update the 'date' field to use the DateInput widget
+        self.fields['date'].widget = forms.DateInput(attrs={'type': 'date'})
--- exercise_project/exercise_app/views.py
+++ exercise_project/exercise_app/views.py
@@ -6,11 +6,21 @@ from .models import Exercise
 
 
 def add_exercise(request):
-    context = {'form': AddExerciseForm()}
-    return render(request, 'exercise_app/add_exercise.html', context)
+    if request.method == "POST":
+        form = AddExerciseForm(request.POST)
+        if form.is_valid():
+            new_exercise = form.save()
+            print("Form valid: ", new_exercise)
+            return redirect('success_page')  # Change 'success_page' to the actual URL name
+        else:
+            print("Failed to validate form")
+    else:
+        context = {'form': AddExerciseForm()}
+        return render(request, 'exercise_app/add_exercise.html', context)
 
 def exercise_list(request):
     exercises = Exercise.objects.all()  # Replace 'Exercise' with your model name
+    print(exercises)
     return render(request, 'exercise_app/exercise_list.html', {'exercises': exercises})
 
 
