e1e49f0a56c3c2713db92ec977a0af9540fa5cc0
kenzen92 <jamespeterkenny@googlemail.com>
2023-10-24 13:15:35 +0100

    changing anything

--- exercise_project/exercise_app/urls.py
+++ exercise_project/exercise_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path("add-exercise", views.add_exercise, name="add-exercise"),
-    path('exercise-list/', views.exercise_list, name='exercise-list'),
+    path('exercise-list', views.exercise_list, name='exercise-list'),
     path("", views.index, name="index"),
 ]
 
