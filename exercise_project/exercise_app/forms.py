e10a2803e2ece7cdf84573590a18a428987637e6
kenzen92 <jamespeterkenny@googlemail.com>
2023-10-24 16:14:53 +0100

    working well, now to add React

--- exercise_project/exercise_app/forms.py
+++ exercise_project/exercise_app/forms.py
@@ -5,6 +5,7 @@ from crispy_forms.layout import Submit
 from django.urls import reverse_lazy
 from .models import Exercise
 
+
 class AddExerciseForm(forms.ModelForm):
     class Meta:
         model = Exercise
@@ -13,7 +14,7 @@ class AddExerciseForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper(self)
-        self.helper.form_action = reverse_lazy('index')
+        self.helper.form_action = reverse_lazy('add-exercise')
         self.helper.add_input(Submit('submit', 'Submit'))
         # Update the 'date' field to use the DateInput widget
         self.fields['date'].widget = forms.DateInput(attrs={'type': 'date'})
