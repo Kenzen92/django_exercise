e62fb5232e560cde1cc7548813ada6d375316175
kenzen92 <jamespeterkenny@googlemail.com>
2023-10-31 15:26:17 +0000

    transfer

--- exercise_project/exercise_project/settings.py
+++ exercise_project/exercise_project/settings.py
@@ -133,6 +133,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
+LOGIN_REDIRECT_URL = '/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -140,3 +142,6 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# preventing email send from mattering
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
