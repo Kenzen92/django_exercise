d9d04a3bc4296aacdc83b97139a201aba82b453b
kenzen92 <jamespeterkenny@googlemail.com>
2023-10-31 15:00:11 +0000

    confused

--- exercise_project/exercise_app/urls.py
+++ exercise_project/exercise_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path("add-exercise", views.add_exercise, name="add-exercise"),
     path('exercise-list/', views.exercise_list, name='exercise-list'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path("", views.index, name="index"),
 ]
 
